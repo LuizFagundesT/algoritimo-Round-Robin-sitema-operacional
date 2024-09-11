21d92229a4fbf93aa16615dcbcc27866171ea547
Luiz Gustavo Fagundes Teixeira <luizagustinho032@gmail.com>
2024-09-11 00:37:14 -0300

    inicializando arquivos + classe Processo

---
+++ cpuRR.js
@@ -0,0 +1,1 @@
+import { Processo } from "./processo";
---
+++ processo.js
@@ -0,0 +1,14 @@
+export class Processo {
+    
+    
+    //criando cosntructor
+    constructor(indiceProcesso,tempoDeSurto ){
+        this.indiceProcesso = indiceProcesso;
+        this.tempoDeSurto=tempoDeSurto;
+    }
+
+    
+
+
+
+}
