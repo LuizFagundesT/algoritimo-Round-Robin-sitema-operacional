4c3621497addf92b88a63d755067e0166e263188
Luiz Gustavo Fagundes Teixeira <luizagustinho032@gmail.com>
2024-09-12 01:34:58 -0300

    não consegui exportar e importar as claases não sei por qual motivo, as coloquei no arquivo principal

--- cpuRR.js
+++ cpuRR.js
@@ -1,1 +1,33 @@
-import { Processo } from "./processo";
+ 
+ //declarando minhas classes
+ class Processo {
+    
+    
+    //criando cosntructor
+    constructor(indiceProcesso,tempoDeSurto ){
+        this.indiceProcesso = indiceProcesso;
+        this.tempoDeSurto=tempoDeSurto;
+    }
+
+   
+}
+
+
+
+
+
+// Função que salva os dados quantum e quantidade de processos
+function salvaDadosAlgoritimo() {
+   // Capturando o valor do input com id 'tempo-surto'
+   var quantum = document.querySelector('#tempo-surto').value;
+   
+   // Exibindo o valor em um alert
+   alert(quantum);
+}
+
+// Capturando o botão 
+const botaoSalvaDadosAlgoritimo = document.querySelector('#btn-salva-dados');
+
+// Adicionando o event listener ao botão
+botaoSalvaDadosAlgoritimo.addEventListener('click', salvaDadosAlgoritimo);
+alert('uai');
